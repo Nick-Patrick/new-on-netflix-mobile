54e589e0066fc41bccd04912dbc9c6105b3073d2
nickpatrick <n.patrick@outlook.com>
2016-01-30 23:30:49 +0000

    Connect app to firebase. Gets titles and auto updates to see latest results

--- new-on-netflix/www/js/controllers/MovieListCtrl.js
+++ new-on-netflix/www/js/controllers/MovieListCtrl.js
@@ -1,14 +1,43 @@
 angular.module('newOnNetflix.controllers')
-  .controller('MovieListCtrl', function($scope, $stateParams, netflixTitles, DateHelper) {
-    $scope.monthsTitles = [];
+  .controller('MovieListCtrl', function($scope, $stateParams, DateHelper, $firebaseObject, $firebaseArray) {
+    $scope.monthsTitles = {
+      days: [
+        {
+          title: 'fetching'
+        }
+      ]
+    };
     setMonths();
     getViewMonth();
     getMonthsTitles();
 
     function getMonthsTitles () {
-      console.log(netflixTitles);
-      $scope.monthsTitles = netflixTitles[$scope.thisMonth.month.toLowerCase() + $scope.thisMonth.year];
-      console.log($scope.monthsTitles);
+      var netflixFirebase = new Firebase('https://netflixtitles.firebaseio.com/netflix/months/' + $scope.thisMonth.month.toLowerCase() + $scope.thisMonth.year);
+      $scope.data = $firebaseArray(netflixFirebase);
+
+      $scope.data.$loaded()
+        .then(function () {
+          $scope.monthsTitles = $scope.data;
+          //$scope.monthsTitles = $scope.data[$scope.thisMonth.month.toLowerCase() + $scope.thisMonth.year];
+          console.log('big->', $scope.monthsTitles);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      netflixFirebase.on("value", function(snapshot) {
+        $scope.$apply(function () {
+          $scope.data = snapshot.val();
+          $scope.monthsTitles = $scope.data;
+          //$scope.monthsTitles = $scope.data.months[$scope.thisMonth.month.toLowerCase() + $scope.thisMonth.year];
+          console.log($scope.monthsTitles);
+        });
+      });
+
+
+      //console.log(netflixTitles);
+      //$scope.monthsTitles = netflixTitles[$scope.thisMonth.month.toLowerCase() + $scope.thisMonth.year];
+      //console.log($scope.monthsTitles);
     }
 
     function getViewMonth () {
